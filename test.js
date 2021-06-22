68fab2fcdc82e7f66893243b9bd6070fa1bde8fc
va1na1 <77720954+va1na1@users.noreply.github.com>
2021-06-22 11:56:35 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,14 @@
+u1 =("va1na1")
+u1p=('hahatestgobrr')
+login= window.prompt('Username')
+if(login==u1) {
+    pass= window.prompt('password')
+}
+else{
+    document.write('Username not found')
+}
+
+if(pass==u1p) {
+    console.log('login succesful')
+    location.href = "tellermp.html";
+}
